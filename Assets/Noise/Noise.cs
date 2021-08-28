ea67d1557f42da6f330171beab894eb7bc1fd350
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-08-28 09:55:48 -0400

    Created Noise Interface

---
+++ Assets/Noise/Noise.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+///     Noise interface defines the required method for a Noise object
+/// </summary>
+public interface Noise
+{
+    /// <summary>
+    ///     sample method gets a sample from a noise object
+    /// </summary>
+    /// <param name="pos">float array of position of sample</param>
+    /// <returns>float of sample value</returns>
+    float sample(float[] pos);
+}
