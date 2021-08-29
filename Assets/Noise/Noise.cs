975113f82089a1d9706b280e7ba95d40b9eefe1a
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-08-29 17:01:28 -0400

    Added toString in Noise interface

--- Assets/Noise/Noise.cs
+++ Assets/Noise/Noise.cs
@@ -9,4 +9,10 @@ public interface Noise
     /// <param name="pos">float array of position of sample</param>
     /// <returns>float of sample value</returns>
     float sample(float[] pos);
+
+    /// <summary>
+    ///     toString method returns a repsentation of noise in a string format
+    /// </summary>
+    /// <returns>string repsentation of noise</returns>
+    string toString();
 }
