ee1546675ce7fa1b8ea04bc5d4c8a4c84bb8bb57
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-08-28 22:38:56 -0400

    Update GroundManager.cs

--- Assets/ground/scripts/monoObjects/GroundManager/GroundManager.cs
+++ Assets/ground/scripts/monoObjects/GroundManager/GroundManager.cs
@@ -17,13 +17,13 @@ public class GroundManager : MonoBehaviour
     int[] samples = new int[] {10, 10};
     int height = 10;
     float bias = 1;
-    float amplitude = 7f / 9f;
+    float amplitude = 5f/9f;
     float[] start = new float[2] { 0.2f, 0.2f };
     float[] end = new float[2] { 1.8f, 1.8f };
 
     void Start()
     {
-        TwoDimensionalNoiseHeightMap twoDimensionalNoiseHeightMap = new TwoDimensionalNoiseHeightMap(NoiseVectors.TwoDimensionSet1, 0, new int[] { 3, 3 }, shaderList.Noise);
+        TwoDimensionalNoiseHeightMap twoDimensionalNoiseHeightMap = new TwoDimensionalNoiseHeightMap(NoiseVectors.TwoDimensionSet1, 0, new int[] { 3, 3 });
         Debug.Log(twoDimensionalNoiseHeightMap.toString());
 
         this.generate(twoDimensionalNoiseHeightMap, samples, height, bias, amplitude, start, end);
