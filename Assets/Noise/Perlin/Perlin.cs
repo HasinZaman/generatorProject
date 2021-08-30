1adcb88dd7cf4cdfc055927ad91bcfbf53994e7e
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-08-29 22:24:41 -0400

    Update Perlin.cs

--- Assets/Noise/Perlin/Perlin.cs
+++ Assets/Noise/Perlin/Perlin.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -164,12 +165,167 @@ public abstract class Perlin : Noise
     /// </summary>
     protected System.Random random;
 
+    private bool lastPosCheck(int[] pos)
+    {
+        for(int i1 = 0; i1 < pos.Length; i1++)
+        {
+            if(pos[i1] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void increment(int[] pos, int i1)
+    {
+        if(i1 > pos.Length)
+        {
+            throw new Exception();
+        }
+
+        if(pos[i1] == 0)
+        {
+            pos[i1] = 1;
+        }
+        else
+        {
+            pos[i1] = 0;
+            increment(pos, i1 + 1);
+        }
+    }
+
+    private int calcPos(int[] pos)
+    {
+        int tmp = 0;
+
+        for (int i1 = this.dim - 1; i1 >= 0; i1--)
+        {
+            tmp = 2 * (pos[i1] + tmp);
+        }
+
+        return tmp;
+    }
+
     /// <summary>
     ///     abstract sample method gets noise value at a given position
     /// </summary>
     /// <param name="pos">array of float repsenting a given position</param>
     /// <returns>Sample value at a given position</returns>
-    public abstract float sample(float[] pos);
+    public float sample(float[] samplePos)
+    {
+        if (this.dim != samplePos.Length)
+        {
+            throw new ArgumentException();
+        }
+
+        bool endCond = false;
+
+        float sampleConst = 2.084991f;
+
+        float[][] vectors = new float[1 << this.dim][];
+        float[][] dist = new float[1 << this.dim][];
+        float[] vertexVal = new float[1 << this.dim];
+
+        int[] pos = new int[this.dim];
+
+        float[] interplateTmp1;
+
+        for (int i1 = 0; i1 < this.dim; i1++)
+        {
+            pos[i1] = (int)samplePos[i1];
+        }
+
+        VectorNode pointer = getVector(pos);
+
+        pos = new int[this.dim];
+
+        int posTmp;
+
+        while (lastPosCheck(pos) || endCond)
+        {
+            //converting vector position into array index
+            posTmp = calcPos(pos);
+
+            //get vector at position
+            vectors[posTmp] = getVector(pos, pointer).val();
+
+            //get distance from node
+            dist[posTmp] = new float[this.dim];
+            
+            for(int i1 = 0; i1 < this.dim; i1++)
+            {
+                dist[posTmp][i1] = samplePos[i1] % 1 - pos[i1];
+            }
+
+            //geting vertVal
+            vertexVal[posTmp] = dotProduct(vectors[posTmp], dist[posTmp]);
+
+
+            if(lastPosCheck(pos) && !endCond)
+            {
+                endCond = true;
+            }
+            else if(endCond)
+            {
+                break;
+            }
+            else
+            {
+                increment(pos, 0);
+            }
+        }
+
+        int dimTmp = this.dim - 1;
+
+        interplateTmp1 = new float[1 << dimTmp];
+
+        pos = new int[this.dim];
+
+        int i = 0;
+
+        ArraySegment<int> splice;
+        int[] spliceArray;
+        int[][] tmpArray = new int[][] { new int[] { 0 }, new int[] { 1 } };
+        
+        while (dimTmp / 2 != 1)
+        {
+            for(int i1 = 0; i1 < pos.Length; i1++)
+            {
+                pos[i + i1] = 0;
+            }
+            endCond = false;
+
+
+
+            while (lastPosCheck(pos) || endCond)
+            {
+                splice = new ArraySegment<int>(pos, i, pos.Length);
+                spliceArray = splice.ToArray();
+                vertexVal[calcPos(spliceArray)] =
+                    cosineInterpolate
+                    (
+                        vertexVal[calcPos(tmpArray[0].Concat(spliceArray).ToArray())],
+                        vertexVal[calcPos(tmpArray[1].Concat(spliceArray).ToArray())],
+                        pos[i] % 1
+                    );
+
+                if (lastPosCheck(pos) && !endCond)
+                {
+                    endCond = true;
+                }
+                else if (endCond)
+                {
+                    break;
+                }
+                else
+                {
+                    increment(pos, i);
+                }
+            }
+        }
+        return (cosineInterpolate(vertexVal[0], vertexVal[1], samplePos[samplePos.Length - 1] % 1) + sampleConst / 2f) / sampleConst;
+    }
 
     /// <summary>
     ///     dotProduct returns the dotProduct of two vectors
@@ -207,7 +363,6 @@ public abstract class Perlin : Noise
         return y1 * (1 - mu) + y2 * mu;
     }
 
-
     /// <summary>
     ///     generateVectors is an abstract method to randomly generate perlin noise vector nodes
     /// </summary>
@@ -215,6 +370,21 @@ public abstract class Perlin : Noise
     /// <param name="end">int array that stores the ending position at which nodes will stop generating</param>
     /// <param name="template">array of perlin noise vectors</param>
     public abstract void generateVectors(int[] start, int[] end);
+
+    // <summary>
+    ///     getVector gets the perlin noise vector at a given position
+    /// </summary>
+    /// <param name="pos">int array of the position of the perlin noise vector</param>
+    /// <returns>float array of noise vector</returns>
+    protected abstract VectorNode getVector(int[] pos);
+
+    /// <summary>
+    ///     getNode gets the perlin noise noise at a given position
+    /// </summary>
+    /// <param name="pos">int array of the position of the perlin noise vector relative to startNode</param>
+    /// <param name="startNode">Vector2DNode instance is the start position</param>
+    /// <returns>Vector2DNode of noise vector at pos</returns>
+    protected abstract VectorNode getVector(int[] pos, VectorNode start);
 
     /// <summary>
     ///     toString method returns a repsentation of perlin noise in a string format
