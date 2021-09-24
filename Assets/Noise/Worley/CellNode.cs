c4746d813561714ee0c21374bf0820c157152724
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-09-24 10:23:27 -0400

    Created CellNode class

---
+++ Assets/Noise/Worley/CellNode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public abstract class CellNode
+{
+    protected float[] vector;
+    protected int dim;
+
+    public CellNode(int dim, float[] val)
+    {
+        this.dim = dim;
+
+        this.set(val);
+    }
+
+    public void set(float[] vector)
+    {
+        if (vector != null)
+        {
+            if (dim != vector.Length)
+            {
+                throw new ArgumentException($"vector lenght must have exactly {dim} elements");
+            }
+            this.vector = vector;
+        }
+    }
+
+    public float[] val()
+    {
+        return this.vector;
+    }
+
+    public abstract CellNode get(int axis, int relativePos);
+
+    public string toString()
+    {
+        string temp = "(";
+
+        temp += $"{this.vector[0]}";
+
+        for (int i1 = 1; i1 < this.vector.Length; i1++)
+        {
+            temp += $",{this.vector[i1]}";
+        }
+
+        temp += ")";
+
+        return temp;
+    }
+}
