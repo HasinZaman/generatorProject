2e680f92e8cd3dff3a63fb95a39bb1009ce2eab0
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-09-24 10:34:49 -0400

    Added Documentation to CellNode.cs

--- Assets/Noise/Worley/CellNode.cs
+++ Assets/Noise/Worley/CellNode.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 
+/// <summary>
+///     CellNode class is an abstract Node class for Worley Noise
+/// </summary>
 public abstract class CellNode
 {
+    /// <summary>
+    ///     vector instances stores the positon of point contained in the cell. The position is measured from the lower left corner.
+    /// </summary>
     protected float[] vector;
+
+    /// <summary>
+    ///     dim defines the number of dimension the point and cell have.
+    /// </summary>
     protected int dim;
 
+    /// <summary>
+    ///     Constructor method sets up the foundation of every CellNode class
+    /// </summary>
+    /// <param name="dim">dim defines the number of dimensions the instance will have</param>
+    /// <param name="val">val is the value given to the vector instance</param>
     public CellNode(int dim, float[] val)
     {
         this.dim = dim;
@@ -12,6 +27,10 @@ public abstract class CellNode
         this.set(val);
     }
 
+    /// <summary>
+    ///     set method handles the re-assignment of vector instance
+    /// </summary>
+    /// <param name="vector">vector is the new value of vector instance</param>
     public void set(float[] vector)
     {
         if (vector != null)
@@ -24,13 +43,27 @@ public abstract class CellNode
         }
     }
 
+    /// <summary>
+    ///     val method returns the value vector instance
+    /// </summary>
+    /// <returns>float array of vector instance</returns>
     public float[] val()
     {
         return this.vector;
     }
 
+    /// <summary>
+    ///     get method returns neighbor cell relative to the current cell
+    /// </summary>
+    /// <param name="axis">int that defines the axis the neighbor node resides on</param>
+    /// <param name="relativePos">int that defines whether to get the neighbor node that is behind/infront of the cell in selected axis</param>
+    /// <returns>neighbor CellNode object</returns>
     public abstract CellNode get(int axis, int relativePos);
 
+    /// <summary>
+    ///     toString method converts vector into a string
+    /// </summary>
+    /// <returns>String repersentation of object</returns>
     public string toString()
     {
         string temp = "(";
