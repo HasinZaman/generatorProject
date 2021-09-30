f2343089be0b9067b2eb2c58f90e907aba6f08ae
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-09-30 12:52:46 -0400

    Created FileReaderGenerator.cs class

---
+++ Assets/ground/scripts/heightMapGeneration/FileReader/FileReaderGenerator.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+///     FileReaderGenerator abstract class handles the foundation of child class that creates chunks from a file
+/// </summary>
+public abstract class FileReaderGenerator : HeightMapGenerator<Grid>
+{
+    static string folder = "";
+    static string fileExtension = "";
+
+    /// <summary>
+    ///     fileName is the name of file that will be read from
+    /// </summary>
+    private string fileName;
+
+    /// <summary>
+    ///     constructor sets up FileReaderGenerator object
+    /// </summary>
+    /// <param name="fileName">name of the file that will be read</param>
+    public FileReaderGenerator(string fileName)
+    {
+        this.fileName = fileName;
+    }
+
+    /// <summary>
+    ///     getHeightMap generates a Grid object that contains the nodes for a HeightMap
+    /// </summary>
+    /// <returns>
+    ///     A grid with the HeightMap data
+    /// </returns>
+    public abstract Grid getHeightMap(object param);
+}
