56fc08e9dba9e471a76e663a4a6f4736e74d8057
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-10-03 19:25:50 -0400

    Created NodeFactory.cs

---
+++ Assets/ground/scripts/heightMapGeneration/FileReader/NodeFactory.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+///     NodeReader co
+/// </summary>
+/// <typeparam name="T">is a generic that repersents </typeparam>
+public interface NodeFactory<T> where T : Node
+{
+    /// <summary>
+    ///     create method converts data into a Node or Node Child
+    /// </summary>
+    /// <param name="raw">string representation of Node</param>
+    /// <returns>Node or Node Child with data stored in raw</returns>
+    T create(string raw);
+}
