bdc6954d2b4c04719f64a842312b7de2207212a7
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-10-03 19:40:27 -0400

    Created ChunkFileReader.cs

---
+++ Assets/ground/scripts/heightMapGeneration/FileReader/Chunk/ChunkFileReader.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+///     ChunkFileReader turns a ".world" into a chunks
+/// </summary>
+public class ChunkFileReader<NF, N, G> : FileReaderGenerator<NF, N, G> where NF : NodeFactory<N> where N : Node where G : Grid
+{
+    public ChunkFileReader() : base(".world", "worlds")
+    {
+
+    }
+
+    public override G getHeightMap(FileParam param)
+    {
+        throw new System.NotImplementedException();
+    }
+}
