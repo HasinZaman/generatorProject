029c1d5d39b684cd004cf9f32abd17448b304ef4
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-10-03 19:22:58 -0400

    Created FileParam.cs

---
+++ Assets/ground/scripts/heightMapGeneration/FileReader/FileParam.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+///     FileParam stores paramater values to create a height map from a file
+/// </summary>
+public struct FileParam
+{
+    /// <summary>
+    ///     fileName is a string of the file that is being loaded
+    /// </summary>
+    public string fileName;
+}
