bb6a500cf90ed2e64649d13f3378f2c46332ec57
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-10-04 16:42:42 -0400

    Added libaries to ChunkFileReader.cs

--- Assets/ground/scripts/heightMapGeneration/FileReader/Chunk/ChunkFileReader.cs
+++ Assets/ground/scripts/heightMapGeneration/FileReader/Chunk/ChunkFileReader.cs
@@ -1,4 +1,7 @@
-﻿/// <summary>
+﻿using System;
+using System.IO;
+
+/// <summary>
 ///     ChunkFileReader turns a ".world" into a chunks
 /// </summary>
 public class ChunkFileReader<NF, N, G> : FileReaderGenerator<NF, N, G> where NF : NodeFactory<N> where N : Node where G : Grid
