fc81a5ddd3daaa34866f7a8bb82293a6f64b63d1
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-10-10 15:34:00 -0400

    Created CreateFile method in FileWriter.cs

--- Assets/Generic/FileWriter.cs
+++ Assets/Generic/FileWriter.cs
@@ -1,2 +1,30 @@
+﻿using System.IO;
+
+/// <summary>
+///     FileWriter creates a file
+/// </summary>
 public class FileWriter
+{
+    /// <summary>
+    ///     static method that creats a file
+    /// </summary>
+    /// <param name="name">string of file name</param>
+    /// <param name="extension">string of file extension</param>
+    /// <param name="directory">string of directory the file is location</param>
+    /// <param name="content">String of the contents of file</param>
+    public static void createFile(string name, string extension, string directory, string content)
+    {
+        File.WriteAllText($"{directory}\\{name}.{extension}", content);
+    }
+
+    /// <summary>
+    ///     static method that creats a file
+    /// </summary>
+    /// <param name="name">string of file name</param>
+    /// <param name="extension">string of file extension</param>
+    /// <param name="content">String of the contents of file</param>
+    public static void createFile(string name, string extension, string content)
+    {
+        File.WriteAllText($"{name}.{extension}", content);
+    }
 }
