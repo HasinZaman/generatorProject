db924b4d9de5688d144e6a85d90d61017d360188
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-10-10 15:13:30 -0400

    Update GroundManager.cs

--- Assets/ground/scripts/monoObjects/GroundManager/GroundManager.cs
+++ Assets/ground/scripts/monoObjects/GroundManager/GroundManager.cs
@@ -12,13 +12,13 @@ public class GroundManager : MonoBehaviour
     public GameObject chunkPrefab;
 
     Chunk[] chunks;
-    int[] chunkDim = new int[2] { 1, 1 };
+    int[] chunkDim = new int[2] { 4, 4 };
 
     float[] nodeDistTemplate = new float[] { 1, 1, 1 };
     int[] gridDim = new int[] { 4, 4, 4 };
 
     int[] samples = new int[] {10, 10};
-    int height = 10;
+    int height = 40;
     float[] start = new float[2] { 0.1f, 0.1f };
     float[] end = new float[2] { 0.9f, 0.9f };
 
@@ -32,30 +32,30 @@ public class GroundManager : MonoBehaviour
 
         public noise(float[][] templateVector, int seed, int[] perlinVectorDim)
         {
-            //perlin2D = new Perlin2D(templateVector, seed, perlinVectorDim);
-            perlin3D = new Perlin3D(NoiseVectors.ThreeDimensionSet2, seed, perlinVectorDim);
+            perlin2D = new Perlin2D(templateVector, seed, perlinVectorDim);
+            //perlin3D = new Perlin3D(NoiseVectors.ThreeDimensionSet2, seed, perlinVectorDim);
         }
 
         public float sample(float[] pos)
         {
-            float[] sample = new float[] { pos[0], pos[1], pos[2]};
+            float[] sample = new float[] { pos[0], pos[2]/*, pos[2]*/};
 
             float[] coord = new float[] { pos[3], pos[4], pos[5] };
 
-            float tmp = perlin3D.sample(sample);
-
-            return (tmp * 1f - 0.0f);
+            float tmp = perlin2D.sample(sample);
+            
+            return (tmp * 9f - coord[1] + 1);
         }
 
         public string toString()
         {
-            return perlin3D.toString();
+            return perlin2D.toString();
         }
     }
 
     void Start()
     {
-        Noise n = new noise(NoiseVectors.ThreeDimensionSet1, 0, new int[] { 10, 10, 10 });
+        Noise n = new noise(NoiseVectors.TwoDimensionSet1, 0, new int[] { 10, 10 });
 
         NoiseHeightMapGenerator noiseHeightMapGenerator = new NoiseHeightMapGenerator(n);
         Debug.Log(n.toString());
@@ -85,7 +85,7 @@ public class GroundManager : MonoBehaviour
     ///     generate method creates the chunks required for the ground
     /// </summary>
     /// <param name="HeightMapGenerator">HeightMapGenerator object that generates height map</param>
-    public void generate(HeightMapGenerator<Grid> HeightMapGenerator)
+    public void generate(HeightMapGenerator<Grid, NoiseHeightMapGenerator.NoiseParam> HeightMapGenerator)
     {
         NoiseHeightMapGenerator.NoiseParam param = new NoiseHeightMapGenerator.NoiseParam();
 
@@ -109,20 +109,22 @@ public class GroundManager : MonoBehaviour
 
         for (int x1 = 0; x1 < chunkDim[0]; x1++)
         {
-            for (int y1 = 0; y1 < chunkDim[1]; y1++)
+            for (int z1 = 0; z1 < chunkDim[1]; z1++)
             {
-                param.start = new float[] { start[0] + delta[0] * x1, start[1] + delta[1] * y1, 0 };
-                param.end = new float[] { start[0] + delta[0] * (x1 + 1), start[1] + delta[1] * (y1 + 1), 0.8f };
+                param.start =   new float[] { start[0] + delta[0] * x1,         0,      start[1] + delta[1] * z1 };
+                param.end =     new float[] { start[0] + delta[0] * (x1 + 1),   0.9f,   start[1] + delta[1] * (z1 + 1) };
+
 
                 chunk = Instantiate(chunkPrefab, this.transform);
 
-                chunk.name = $"({x1},{y1})";
-                chunk.transform.position = new Vector3(x1 * nodeDistTemplate[0] * (10 - 1), 0, y1 * nodeDistTemplate[2] * (10 - 1));
+                chunk.name = $"({x1},{z1})";
 
-                chunks[x1 + y1 * chunkDim[0]] = chunk.GetComponent<Chunk>();
+                chunk.transform.position = new Vector3(x1 * nodeDistTemplate[0] * (10 - 1), 0, z1 * nodeDistTemplate[2] * (10 - 1));
 
-                chunks[x1 + y1 * chunkDim[0]].setChunk(HeightMapGenerator.getHeightMap(param), shaderList.MarchingCube, nodeDistTemplate);
-                Debug.Log(chunks[x1 + y1 * chunkDim[0]].toString());
+                chunks[x1 + z1 * chunkDim[0]] = chunk.GetComponent<Chunk>();
+
+                chunks[x1 + z1 * chunkDim[0]].setChunk(HeightMapGenerator.getHeightMap(param), shaderList.MarchingCube, nodeDistTemplate);
+                //Debug.Log(chunks[x1 + y1 * chunkDim[0]].toString());
             }
         }
     }
