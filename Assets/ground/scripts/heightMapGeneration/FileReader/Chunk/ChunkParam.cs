3cb1da9297f53e861697d4fe2cb904302060e514
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-10-12 19:21:11 -0400

    Created ChunkParam.cs

---
+++ Assets/ground/scripts/heightMapGeneration/FileReader/Chunk/ChunkParam.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+///     ChunkParam is paramater used to load .world files
+/// </summary>
+public class ChunkParam : FileParam
+{
+    /// <summary>
+    ///     string of ChunkId
+    /// </summary>
+    public string chunkId;
+}
