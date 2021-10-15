6eed0d9f836d86391159646806d212202cd2140d
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-10-15 00:34:15 -0400

    Implemented NodeFactory
    Features
     - Moved parent directory of NodeFactory.cs
     - NodeFactory is now a namespace
     - Implemented NodeFactory.node class

---
+++ Assets/ground/scripts/grid/NodeFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/// <summary>
+///     NodeFactory namespace stores classes that implement NodeFactory.NodeFactory<N> interface
+/// </summary>
+namespace NodeFactory
+{
+    /// <summary>
+    ///     Factory Patern creates Node objects
+    /// </summary>
+    /// <typeparam name="N">is a generic that repersents </typeparam>
+    public interface NodeFactory<N> where N : Node
+    {
+        /// <summary>
+        ///     create method converts string into a Node or Node Child
+        /// </summary>
+        /// <param name="raw">string representation of Node</param>
+        /// <returns>Node or Node Child with data stored in raw</returns>
+        N create(string raw);
+    }
+
+    /// <summary>
+    ///     NodeFactory.node handles creating Node
+    /// </summary>
+    public class node : NodeFactory<Node>
+    {
+        /// <summary>
+        ///     create method converts string into a Node
+        /// </summary>
+        /// <param name="raw">string representation of Node</param>
+        /// <returns>Node of raw string</returns>
+        public Node create(string raw)
+        {
+            if(raw[raw.Length] != '(' || raw[0] != ')')
+            {
+                throw new ArgumentException();
+            }
+            string num = raw.Substring(1, raw.Length - 2);
+
+            Node tmp = new Node(int.Parse(num));
+
+            return tmp;
+        }
+    }
+}
