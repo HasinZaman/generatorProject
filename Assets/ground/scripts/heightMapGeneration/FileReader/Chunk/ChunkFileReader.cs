a4a6b478e9a42671ca063fb1026d776a017ef5d4
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-10-15 00:34:36 -0400

    Update ChunkFileReader.cs

--- Assets/ground/scripts/heightMapGeneration/FileReader/Chunk/ChunkFileReader.cs
+++ Assets/ground/scripts/heightMapGeneration/FileReader/Chunk/ChunkFileReader.cs
@@ -7,24 +7,49 @@ using System.IO;
 /// <typeparam name="NF">Node Factory class/child class</typeparam>
 /// <typeparam name="N">Node class/child class</typeparam>
 /// <typeparam name="G">Grid class/child class</typeparam>
-public class ChunkFileReader<NF, N, G> : FileReaderGenerator<NF, N, G, ChunkParam> where NF : NodeFactory<N> where N : Node where G : Grid
+public class ChunkFileReader<NF, N> : FileReaderGenerator<NF, N, Grid, ChunkParam> where NF : NodeFactory.NodeFactory<N> where N : Node
 {
+
+    private string saveName = null;
+
+    private NF nodeFactory;
+
     /// <summary>
     ///     Constructor sets up ChunkFileReader object
     /// </summary>
-    public ChunkFileReader() : base(".world", "worlds")
+    public ChunkFileReader(NF nodeFactory) : base(".chunk", "worlds")
     {
-
+        this.nodeFactory = nodeFactory;
     }
 
     /// <summary>
-    ///     stringToHeightMap converts a string into a grid object
+    ///     Constructor sets up ChunkFileReader object
     /// </summary>
-    /// <param name="gridRaw">String that will proccessed into a grid object</param>
-    /// <returns>Grid object of gridRaw string</returns>
-    private G stringToHeightMap(string gridRaw)
+    /// <param name="saveName">name of save file</param>
+    public ChunkFileReader(string saveName) : base(".chunk", "worlds")
     {
-        throw new ArgumentException();
+        this.saveName = saveName;
+    }
+
+    /// <summary>
+    ///     setSave method assign value to saveName
+    /// </summary>
+    /// <param name="saveName">name of save file</param>
+    public void setSave(string saveName)
+    {
+        this.saveName = saveName;
+    }
+
+    private int[] stringToInt(string str)
+    {
+        string[] tmp1 = str.Split(',');
+        int[] tmp2 = new int[tmp1.Length];
+        for (int i1 = 0; i1 < tmp2.Length; i1++)
+        {
+            tmp2[i1] = int.Parse(tmp1[i1]);
+        }
+
+        return tmp2;
     }
 
     /// <summary>
@@ -34,17 +59,45 @@ public class ChunkFileReader<NF, N, G> : FileReaderGenerator<NF, N, G, ChunkPara
     /// <returns>
     ///     A grid with the HeightMap data
     /// </returns>
-    public override G getHeightMap(ChunkParam param)
+    public override Grid getHeightMap(ChunkParam param)
     {
-        if (!validFileCheck(param.fileName))
+        if (!validFileCheck($"{this.saveName}\\{param.fileName}.{this.fileExtension}"))
         {
             throw new ArgumentException("File Does not exist");
         }
 
-        throw new NotImplementedException();
+        string strTmp = File.ReadAllText($"{this.folder}\\{this.saveName}\\{param.fileName}.{this.fileExtension}");
+
+        string[] tmp;
+        string[] rawNodes;
+        int[] dim, pos;
+
+        Node[] nodes;
+
+        tmp = strTmp.Split('|');
+
+        //assigning dim
+        dim = stringToInt(tmp[0]);
+        pos = new int[2] { 0, 0 };
+
+        nodes = new N[tmp.Length - 1];
+
+        for (int i1 = 1; i1 < tmp.Length; i1++)
+        {
+            rawNodes = tmp[i1].Split(',');
+            for (int i2 = 0; i2 < rawNodes.Length; i2++)
+            {
+                nodes[pos[0] + dim[0] * (pos[1] + dim[1] * i2)] = (Node)nodeFactory.create(rawNodes[i2]);
+            }
+
+            pos[1] = (pos[1] + (pos[0] + 1) / dim[1]) % dim[1];
+            pos[0] = (pos[0] + 1) % dim[0];
+        }
+
+        return new Grid(nodes, dim);
     }
 
-    public override G[] getHeightMaps(ChunkParam param)
+    public override Grid[] getHeightMaps(ChunkParam param)
     {
         throw new NotImplementedException();
     }
