f57c26e0700e6ebfb3c8680951f01c95a6d35320
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-10-16 11:18:50 -0400

    Created ImageFileReaderGenerator.cs

---
+++ Assets/ground/scripts/heightMapGeneration/FileReader/ImageFile/ImageFileReaderGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+
+public class ImageFileReaderGenerator<NF, N> : FileReaderGenerator<NF, N, Grid, ChunkParam> where NF : NodeFactory.NodeFactory<N> where N : Node
+{
+    private NF nodeFactory;
+    public ImageFileReaderGenerator(NF nodeFactory) : base("png", "worlds")
+    {
+        this.nodeFactory = nodeFactory;
+    }
+
+    public override Grid getHeightMap(ChunkParam param)
+    {
+        if (!validFileCheck($"{param.fileName}.{this.fileExtension}"))
+        {
+            throw new ArgumentException($"File \"{getFileAddress($"{param.fileName}.{this.fileExtension}")}\" Does not exist");
+        }
+
+        throw new System.NotImplementedException();
+    }
+
+    public override Grid[] getHeightMaps(ChunkParam param)
+    {
+        throw new System.NotImplementedException();
+    }
+}
