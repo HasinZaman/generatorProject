865fe91e83a390ca6c04bc0186ffdb29b8746f3e
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-10-16 11:18:59 -0400

    Update ChunkParam.cs

--- Assets/ground/scripts/heightMapGeneration/FileReader/Chunk/ChunkParam.cs
+++ Assets/ground/scripts/heightMapGeneration/FileReader/Chunk/ChunkParam.cs
@@ -4,7 +4,29 @@
 public class ChunkParam : FileParam
 {
     /// <summary>
-    ///     string of ChunkId
+    ///     Coord struct stores x and y position of chunks
     /// </summary>
-    public string chunkId;
+    public struct Coord
+    {
+        public int x;
+        public int y;
+    }
+    
+    public Coord start;
+    public Coord end;
+
+    /// <summary>
+    ///     ChunkParam constructor sets up ChunkParam
+    /// </summary>
+    public ChunkParam()
+    {
+        start = new Coord();
+        start.x = 0;
+        start.y = 0;
+
+        end = new Coord();
+        end.x = 0;
+        end.y = 0;
+    }
+
 }
