2aaa8ffc07b6abb8432b8d9d1d81414bf1386679
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-10-22 16:55:08 -0400

    Moved Chunk management methods from GroundManager.cs to ChunkManager.cs

--- Assets/ground/scripts/monoObjects/GroundManager/ChunkManager/ChunkManager.cs
+++ Assets/ground/scripts/monoObjects/GroundManager/ChunkManager/ChunkManager.cs
@@ -1,1 +1,68 @@
+﻿using System;
+using UnityEngine;
+
+public class ChunkManager
+{
+    float[] chunkDist = new float[3];
+
+    float loadDist;
+    float deLoadDist;
+    float renderDist;
+    float deRenderDist;
+
+    public GameObject centerObj;
+    public int[] pos = new int[2] { 0, 0 };
+
+    GameObject[] renderedChunks;
+    Grid[] loadedChunks;
+    ChunkFileReader<NodeFactory.node, Node> chunkFileReader;
+
+    /// <summary>
+    ///     setChunkDist method sets the width, height and length of a chunk
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="z"></param>
+    public void setChunkDist(float x, float y, float z)
+    {
+    }
+
+    /// <summary>
+    ///     setLoadDist method sets 
+    /// </summary>
+    /// <param name="renderDist"></param>
+    /// <param name="deRenderDist"></param>
+    /// <param name="loadDist"></param>
+    /// <param name="deLoadDist"></param>
+    public void setLoadDist(float renderDist, float deRenderDist, float loadDist, float deLoadDist)
+    {
+    }
+
+    /// <summary>
+    ///     update method loads and deloads chunks
+    /// </summary>
+    public void update()
+    {
+
+    }
+
+    /// <summary>
+    ///     loadChunk creates Grid from .chunk file
+    /// </summary>
+    /// <param name="x">x pos of chunk</param>
+    /// <param name="y">y pos of chunk</param>
+    /// <returns>Grid of .chunk file</returns>
+    public Grid loadChunk(int x, int y)
+    {
+        ChunkParam fileParam = new ChunkParam();
+
+        fileParam.fileName = $"C({x},{y})";
+
+        return chunkFileReader.getHeightMap(fileParam);
+    }
+
+    public void convertChunk(GameObject c, Grid g)
+    {
+        throw new NotImplementedException();
+    }
 }
