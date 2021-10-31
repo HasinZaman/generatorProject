663f602c21618c9545e4e3a41c065352116bdefa
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-10-31 19:17:38 -0400

    Created LinkedStack.cs

---
+++ Assets/Generic/Stack/LinkedStack.cs
@@ -0,0 +1,93 @@
+﻿/// <summary>
+///     Stack implementation with linked elements
+/// </summary>
+/// <typeparam name="E"></typeparam>
+public class LinkedStack<E> : Stack<E>
+{
+    /// <summary>
+    ///     Element class used to link elements
+    /// </summary>
+    private class Element
+    {
+        public E val;
+        public Element next;
+
+        /// <summary>
+        ///     Element Constructor
+        /// </summary>
+        /// <param name="val">value of element</param>
+        /// <param name="next">pointer to the next elment in stack</param>
+        public Element(E val, Element next)
+        {
+            this.val = val;
+            this.next = next;
+        }
+        
+    }
+
+    private int i = 0;
+    private Element root;
+
+    /// <summary>
+    ///     isEmpty returns bool if stack is empty
+    /// </summary>
+    /// <returns>bool of stack is empty</returns>
+    public bool isEmpty()
+    {
+        return i == 0;
+    }
+
+    /// <summary>
+    ///     pop method removes and returns top most element
+    /// </summary>
+    /// <returns>Top most elment</returns>
+    public E pop()
+    {
+        if(i == 0)
+        {
+            return default(E);
+        }
+
+        Element tmp = root;
+        root = root.next;
+        tmp.next = null;
+        i--;
+        return tmp.val;
+    }
+
+    /// <summary>
+    ///     Adds element into the stack
+    /// </summary>
+    /// <param name="elem"></param>
+    public void push(E elem)
+    {
+        Element tmp = new Element(elem, root);
+
+        root = tmp;
+        i++;
+    }
+
+
+    /// <summary>
+    ///     method returns the size of the stack
+    /// </summary>
+    /// <returns>int size of stack</returns>
+    public int size()
+    {
+        return i;
+    }
+
+    /// <summary>
+    ///     top method returns the top most element
+    /// </summary>
+    /// <returns>Top most Element</returns>
+    public E top()
+    {
+        if(root == null)
+        {
+            return default(E);
+        }
+
+        return root.val;
+    }
+}
